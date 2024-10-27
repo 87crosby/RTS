1b78687749213ac414866ccc037b80eca1f78318
87crosby <71791452+87crosby@users.noreply.github.com>
2024-10-27 15:42:33 -0400

    Add api url to frontend

---
+++ client/src/api/index.js
@@ -0,0 +1,18 @@
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+
+export const login = async (credentials) => {
+  const response = await axios.post(`${API_URL}/api/login`, credentials);
+  return response.data;
+};
+
+export const register = async (userData) => {
+  const response = await axios.post(`${API_URL}/api/register`, userData);
+  return response.data;
+};
+
+export const getStockPrice = async (symbol, token) => {
+  const response = await axios.get(`${API_URL}/api/stock/${symbol}`, {
+    headers: { Authorization: `Bearer ${token}` }
+  });
+  return response.data;
+};
